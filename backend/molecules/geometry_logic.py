f45bdb76a8c92e73b8f0779ab3fead993c15a688
AbElo-77 <abdallaelokely@gmail.com>
2025-12-15 23:07:30 -0700

    Harmonic and dihedral representations

--- backend/molecules/geometry_logic.py
+++ backend/molecules/geometry_logic.py
@@ -1,13 +1,59 @@
-import numpy as np 
+import numpy as np
 
-class Angle(): 
+class Angle:
 
-    def __init__(self, atom_1, atom_2, atom_3):
+    def __init__(self, atom_1, atom_2, atom_3, params):
 
-        self.vector_1_2 = (atom_2.position) - (atom_1.position)
-        self.vector_2_3 = (atom_3.position) - (atom_2.position)
+        self.theta0, self.k_constant = params
 
-        cosine_theta = np.dot(self.vector_1_2, self.vector_1_2)
-        cosine_theta = cosine_theta / (np.linalg.norm(self.vector_1_2) * np.linalg.norm(self.vector_1_2))
-        self.theta = np.arccos(cosine_theta)
+        self.vector_1_2 = atom_1.position - atom_2.position
+        self.vector_2_3 = atom_3.position - atom_2.position
 
+    def calculate_value(self): 
+
+        dot = np.dot(self.vector_1_2, self.vector_2_3)
+        norm = (np.linalg.norm(self.vector_1_2) * np.linalg.norm(self.vector_2_3))
+
+        cosine_theta = dot / norm
+        cosine_theta = np.clip(cosine_theta, -1.0, 1.0)
+
+        return np.arccos(cosine_theta)
+
+    def get_plane(self):
+
+        normal_vector = np.cross(self.vector_1_2, self.vector_2_3)
+        norm = np.linalg.norm(normal_vector)
+
+        if norm < 1e-12:
+            return None
+
+        normal_vector /= norm
+        return normal_vector
+    
+    def energy(self): 
+
+        theta = self.calculate_value()
+        return 0.5 * self.k * (theta - self.theta0)**2
+
+class Dihedral:
+
+    def __init__(self, atom_1, atom_2, atom_3, atom_4):
+
+        b1 = atom_2.position - atom_1.position
+        b2 = atom_3.position - atom_2.position
+        b3 = atom_4.position - atom_3.position
+
+        self.plane_1_2_3 = np.cross(b1, b2)
+        self.plane_2_3_4 = np.cross(b2, b3)
+
+        n1_norm = np.linalg.norm(self.plane_1_2_3)
+        n2_norm = np.linalg.norm(self.plane_2_3_4)
+
+        self.plane_1_2_3 /= n1_norm
+        self.plane_2_3_4 /= n2_norm
+
+        b2_hat = b2 / np.linalg.norm(b2)
+        x = np.dot(self.plane_1_2_3, self.plane_2_3_4)
+        y = np.dot(b2_hat, np.cross(self.plane_1_2_3, self.plane_2_3_4))
+
+        self.phi = np.arctan2(y, x)
--- backend/molecules/molecule_assembly.py
+++ backend/molecules/molecule_assembly.py
@@ -1,26 +1,28 @@
 from rdkit import Chem
+import numpy as np
+from backend.molecules.geometry_logic import Angle, Dihedral
 
 class Atom(): 
 
-    def __init__(self, id, element, atomic_number, mass,
-                 formal_charge=0, velocity=[0,0,0], position=(0,0,0)):
+    def __init__(self, id, element,
+                 formal_charge=0, velocity=None, position=None):
         
         self.id = id
-        self.element = Chem.AtomFromSmarts(element)
-        self.atomic_number = atomic_number
+        self.element = Chem.Atom(element)
+        self.atomic_number = self.element.GetAtomicNum()
         self.valence = self.element.GetValence(Chem.ValenceType.EXPLICIT)
-        self.mass = mass
+        self.mass = self.element.GetMass()
 
-        self.velocity = velocity
-        self.position = position
+        zero_array = np.zeros(3)
+        self.velocity = velocity if velocity is not None else zero_array
+        self.position = position if position is not None else zero_array
+
         self.formal_charge = formal_charge
-
-        self.hybridization = (self.valence // 2)
+        self.hybridization = None
     
-    def update_hybridization(self, bonds=(1, 1, 1, 1)):
+    def update_hybridization(self, hybridization):
 
-        num_bonds = len(bonds)
-        self.hybridization = (num_bonds) + ((self.valence - num_bonds) // 2)
+        self.hybridization = hybridization
 
 class Bond(): 
 
@@ -30,8 +32,8 @@ class Bond():
         self.atom_2 = atom_2
 
         self.order = order
-        self.equilibrium_length = Engine.compute() 
-        self.k_force_constant = Engine.compute()
+        # self.equilibrium_length = Engine.compute() 
+        # self.k_force_constant = Engine.compute()
 
         self.rotatable = True
         if order > 1: 
@@ -51,7 +53,13 @@ class Molecule(object):
         self.atoms = atoms
         self.bonds = bonds
 
-        # construct adjacency list 
+        self.adjacency_list = {}
+        for atom in atoms: 
+            adjacent_atoms = []
+            for bond in bonds: 
+                if bond.atom_1 == atom or bond.atom_2 == atom: 
+                    adjacent_atoms.append(bond.atom_2 if bond.atom_1 == atom else bond.atom_1)
+            self.adjacency_list[atom] = adjacent_atoms
 
         self.masses = []
         for atom in atoms: 
@@ -65,25 +73,66 @@ class Molecule(object):
         for atom in atoms: 
             self.positions.append(atom.position)
 
-        self.angles = {}
+        self.bond_angles = {}
+        self.torsion_angles = {}
+
         for atom in atoms: 
-            self.update_angles()
+            self.update_angles(atom)
+
     
     def add_atom(self, id, element, atomic_number, mass, 
-                formal_charge=0, velocity=[0,0,0], position=(0,0,0)):
+                formal_charge=0, velocity=None, position=None):
         
-        new_atom = Atom(id, element, atomic_number, mass, formal_charge, 
+        new_atom = Atom(id, element, formal_charge, 
                             velocity, position)
         self.atoms.append(new_atom)
+        self.adjacency_list[new_atom] = []
 
     def add_bond(self, atom_1, atom_2, bond_order): 
 
         new_bond = Bond(atom_1, atom_2, bond_order)
         self.bonds.append(new_bond)
 
+        self.adjacency_list[atom_1].append(atom_2)
+        self.adjacency_list[atom_2].append(atom_1)
+
         self.update_angles(atom_1)
         self.update_angles(atom_2)
 
     def update_angles(self, atom_pivot): 
 
-        return
+        adjacent_atoms = self.adjacency_list[atom_pivot]
+
+        for atom in adjacent_atoms:
+            start_index = 1
+            for i in range(start_index, len(adjacent_atoms)):
+                if (atom, atom_pivot, adjacent_atoms[i]) in self.bond_angles or (adjacent_atoms[i], atom_pivot, atom) in self.bond_angles:
+                    continue
+
+                params = (0, 0)
+                # params = Engine.get_params(atom, atom_pivot, adjacent_atoms[i])
+
+                angle_new = Angle(atom, atom_pivot, adjacent_atoms[i], params)
+                self.bond_angles[(atom, atom_pivot, adjacent_atoms[i])] = angle_new
+
+        for atom in adjacent_atoms:
+            self.update_dihedrals_for_bond(atom_pivot, atom)
+            
+    def update_dihedrals_for_bond(self, atom_j, atom_k):
+
+        neighbors_j = self.adjacency_list[atom_j]
+        neighbors_k = self.adjacency_list[atom_k]
+
+        for atom_i in neighbors_j:
+            if atom_i == atom_k:
+                continue
+
+            for atom_l in neighbors_k:
+                if atom_l == atom_j:
+                    continue
+
+                key = (atom_i, atom_j, atom_k, atom_l)
+                rev_key = (atom_l, atom_k, atom_j, atom_i)
+
+                if key not in self.torsion_angles and rev_key not in self.torsion_angles:
+                    self.torsion_angles[key] = Dihedral(atom_i, atom_j, atom_k, atom_l)
