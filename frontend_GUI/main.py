c2ae33fc148d6b757ea8ff7bc5deb2a83b35b84f
AbElo-77 <abdallaelokely@gmail.com>
2025-12-25 20:14:07 -0700

    Testing pyqtgraph modeling

--- frontend_GUI/main.py
+++ frontend_GUI/main.py
@@ -2,6 +2,11 @@ import sys
 import PyQt5.QtWidgets as pyqtW
 import PyQt5.QtGui as pyqtG
 import PyQt5.QtCore as pyqtC
+import pyqtgraph as pg
+import pyqtgraph.opengl as og
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
 
 class MainWindow(pyqtW.QMainWindow): 
 
@@ -14,23 +19,60 @@ class MainWindow(pyqtW.QMainWindow):
     def initUI(self): 
         
         self.setWindowTitle("MolecuSim")
-        layout = pyqtW.QGridLayout()
 
-        nav_bar = pyqtW.QMenuBar()
-        nav_bar.setStyleSheet("background: lightgrey; width: 1000px; max-height: 50px;")
-        side_bar = pyqtW.QMenuBar()
-        side_bar.setStyleSheet("background: lightgrey; max-width: 100px; height: 1200px")
+        central = pyqtW.QWidget()
+        layout = pyqtW.QGridLayout()
+        central.setLayout(layout)
+        self.setCentralWidget(central)
+
+        nav_bar = pyqtW.QFrame()
+        nav_bar.setStyleSheet("background: lightgrey; max-height: 50px;")
         layout.addWidget(nav_bar, 0, 1)
+
+        side_bar = pyqtW.QFrame()
+        side_bar.setStyleSheet("background: lightgrey; max-width: 100px;")
         layout.addWidget(side_bar, 0, 0, 2, 1)
 
-        main_window = pyqtW.QMenuBar()
-        main_window.setStyleSheet("background: white; width: 1000px; height: 1000px;")
-        layout.addWidget(main_window, 1, 1)
+        gl_widget = og.GLViewWidget()
+        gl_widget.setBackgroundColor('w')
+        gl_widget.opts['distance'] = 20
 
-        widget = pyqtW.QWidget()
-        widget.setLayout(layout)
+        size = 500
+        spacing = 50
 
-        self.setCentralWidget(widget)
+        grid_xy = og.GLGridItem()
+        grid_xy.setSize(size, size, 1)
+        grid_xy.setColor((1, 1, 1, 1))
+        grid_xy.setSpacing(spacing, spacing, 1)
+        grid_xy.rotate(90, 1, 0, 0)
+        grid_xy.setGLOptions('additive')
+        gl_widget.addItem(grid_xy)
+
+        grid_xz = og.GLGridItem()
+        grid_xz.setSize(size, size, 1)
+        grid_xz.setColor((0, 0, 0, 1))
+        grid_xz.setSpacing(spacing, spacing, 1)
+        grid_xz.setGLOptions('additive')
+        gl_widget.addItem(grid_xz)
+
+        grid_yz = og.GLGridItem()
+        grid_yz.setSize(size, size, 1)
+        grid_yz.setColor((0, 0, 0, 1))
+        grid_yz.setSpacing(spacing, spacing, 1)
+        grid_yz.setGLOptions('additive')
+        grid_yz.rotate(90, 0, 1, 0)
+        gl_widget.addItem(grid_yz)
+
+        axis = og.GLAxisItem()
+        axis.setGLOptions('opaque')
+        sphere = og.MeshData.sphere(20, 40, 3)
+        sphere_item = og.GLMeshItem(meshdata=sphere, smooth=True, color=(0, 0, 0, 0), shader='balloon')
+        sphere_item.translate(1, 2, 3)
+        
+        gl_widget.addItem(axis)
+        gl_widget.addItem(sphere_item)
+
+        layout.addWidget(gl_widget, 1, 1)
 
 
 
